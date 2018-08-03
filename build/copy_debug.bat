c3ac8ff5fd12d91ef0456c2c68eb0f41d33acca0
Soleil Rojas <soleilrojas@gmail.com>
2018-08-03 17:22:54 -0600

    Add more files to build scripts

--- build/copy_debug.bat
+++ build/copy_debug.bat
@@ -5,3 +5,7 @@ xcopy "..\src\login_server\Debug\login_server.exe" "..\bin\login\login_server.ex
 xcopy "..\src\login_server\Debug_NoSQL\login_server.exe" "..\bin\login_nosql\login_server.exe" /Y
 
 xcopy "..\src\mysql\lib\libmysql.dll" "..\bin\login\libmysql.dll" /Y
+
+xcopy "..\src\patch_server\Debug\patch_server.exe" "..\bin\patch\patch_server.exe" /Y
+
+xcopy "..\src\ship_server\Debug\ship_server.exe" "..\bin\ship\ship_server.exe" /Y
--- build/copy_release.bat
+++ build/copy_release.bat
@@ -5,3 +5,7 @@ xcopy "..\src\login_server\Release\login_server.exe" "..\bin\login\login_server.
 xcopy "..\src\login_server\Release_NoSQL\login_server.exe" "..\bin\login_nosql\login_server.exe" /Y
 
 xcopy "..\src\mysql\lib\libmysql.dll" "..\bin\login\libmysql.dll" /Y
+
+xcopy "..\src\patch_server\Release\patch_server.exe" "..\bin\patch\patch_server.exe" /Y
+
+xcopy "..\src\ship_server\Release\ship_server.exe" "..\bin\ship\ship_server.exe" /Y
